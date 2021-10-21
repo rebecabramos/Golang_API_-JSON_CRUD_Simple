2a170ca74538749bdb541a71ad51893ec2237dbf
Rebeca Raab Bias Ramos <41303692+rebecabramos@users.noreply.github.com>
2021-10-21 18:05:26 -0300

    Code Refactoring main.go

--- main.go
+++ main.go
@@ -5,11 +5,22 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Bem vinda")
-	}) // creates routes in this case the root directory because only the (/) was passed
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bem vinda")
+}
+
+func configureHandles() {
+	http.HandleFunc("/", handleMain) // creates routes in this case the root directory because only the (/) was passed
+
+}
+
+func configureServer() {
+	configureHandles()
 
 	fmt.Println("The Server is running on port 1337")
 	http.ListenAndServe(":1337", nil) // run the server http (DefaultServerMux)
+}
+
+func main() {
+	configureServer()
 }
