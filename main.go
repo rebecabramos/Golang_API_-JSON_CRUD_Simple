c85df0df975c0d3020c8d72629b47057e940e1e6
Rebeca Raab Bias Ramos <41303692+rebecabramos@users.noreply.github.com>
2021-10-21 18:40:25 -0300

    Create list books in format json (main.go)

--- main.go
+++ main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type Book struct {
+	Id     int
+	Title  string
+	Author string
+}
+
+var Books []Book = []Book{
+	Book{
+		Id:     1,
+		Title:  "O Guarani",
+		Author: "José de Alencar",
+	},
+	Book{
+		Id:     2,
+		Title:  "Cazuza",
+		Author: "Viriato Correia",
+	},
+}
 
 func handleMain(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Bem vinda")
 }
 
+func listBooks(w http.ResponseWriter, r *http.Request) {
+	encoder := json.NewEncoder(w) // returns a new encoder that writes to w
+	encoder.Encode(Books)
+}
+
 func configureHandles() {
 	http.HandleFunc("/", handleMain) // creates routes in this case the root directory because only the (/) was passed
-
+	http.HandleFunc("/Books", listBooks)
 }
 
 func configureServer() {
